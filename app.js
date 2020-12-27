070656056c145fa870f403289e9d5af7ed3de889
johnpiapian <johnpiapian@gmail.com>
2020-12-27 15:15:50 -0600

    Updated

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3100
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(80, () => {
+  console.log(`Server started!`)
 })
